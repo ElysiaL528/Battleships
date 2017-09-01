5eddedc585e8f875a839a5c961acaaa5d02bfab9
ElysiaL528 <elysialopez528@gmail.com>
2017-09-01 15:59:26 -0700

    Worked on stored procedures. Made minor modifications to existing procedures to accomodate new functionalities.

--- SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
@@ -1,18 +1,23 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 8/31/2017 2:22:14 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 9/1/2017 3:57:23 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE PROC [dbo].[usp_PlaceShip]
 	@ShipID				int
 ,	@UserID				int
 ,	@RoomID				int
-,	@X					int
-,	@Y					int
+,	@X					tinyint
+,	@Y					tinyint
 ,	@ShipOrientationID	int
 as
-INSERT INTO UserShips
-VALUES (@ShipID, @UserID, @RoomID, @X, @Y, @ShipOrientationID)
+if NOT EXISTS(SELECT ShipID FROM UserShips WHERE UserID = @UserID)
+	BEGIN
+		INSERT INTO UserShips
+		VALUES (@ShipID, @UserID, @RoomID, @X, @Y, @ShipOrientationID)
+	END
+
 GO
---
+++ SQL/StoredProcedures/dbo.usp_SetUserReady.StoredProcedure.sql
@@ -0,0 +1,53 @@
+USE [ElysiaLopezBattleships2017]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_SetUserReady]    Script Date: 9/1/2017 3:57:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROC [dbo].[usp_SetUserReady]
+	@UserID int
+,	@RoomID int
+as
+
+
+DECLARE @HostPlayerID int = (SELECT HostPlayerID FROM Rooms WHERE RoomID = @RoomID)
+DECLARE @JoinedPlayerID int = (SELECT JoinedPlayerID FROM Rooms WHERE RoomID = @RoomID)
+DECLARE @UserShipCount int = (SELECT COUNT(ShipID) FROM UserShips WHERE UserID = @UserID)
+
+if(@UserShipCount = 5)
+	BEGIN
+
+		if(@UserID = @HostPlayerID)
+			BEGIN
+				UPDATE Rooms
+				SET isHostPlayerReady = 1
+				WHERE RoomID = @RoomID
+			END
+		
+		else if(@UserID = @JoinedPlayerID)
+			BEGIN
+				UPDATE Rooms
+				SET isJoinedPlayerReady = 1
+				WHERE RoomID = @RoomID
+			END
+	END
+
+SELECT	RoomID
+	,		RoomName
+	,		hp.UserID		AS HostPlayerID
+	,		jp.UserID
+	,		Rooms.isHostPlayerReady
+	,		Rooms.isJoinedPlayerReady
+	FROM	Rooms
+	LEFT JOIN	Users		jp
+	ON		Rooms.JoinedPlayerID	=	jp.UserID
+	JOIN		Users		hp
+	ON		Rooms.HostPlayerID		=	hp.UserID
+	WHERE RoomID	=	@RoomID
+	AND		jp.UserID	=	Rooms.JoinedPlayerID
+
+
+GO
