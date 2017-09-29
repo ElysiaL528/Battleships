b38fc1947c040647b8147456bfb6bc26198ef222
ElysiaL528 <elysialopez528@gmail.com>
2017-09-29 13:56:14 -0700

    Finished FireShot procedure. Created function to check each ship to see if the shot hit or missed. Made minor fixes in other procedures. Close issue #2.

--- SQL/StoredProcedures/dbo.usp_FireShot.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_FireShot.StoredProcedure.sql
@@ -1,21 +1,79 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_FireShot]    Script Date: 9/15/2017 2:49:26 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_FireShot]    Script Date: 9/29/2017 1:53:56 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+
 CREATE PROC [dbo].[usp_FireShot]
 	@UserID		int
 ,	@RoomID		int
-,	@X			int
-,	@Y			int
+,	@ShotX			int	
+,	@ShotY			int		
 as
 
-DECLARE @JoinedPlayerID int = (SELECT JoinedPlayerID FROM Rooms WHERE RoomID = @RoomID)
-DECLARE @HostPlayerID int = (SELECT HostPlayerID FROM Rooms WHERE RoomID = @RoomID)
+--Can be selected from tables
+DECLARE @shipStartX			tinyint
+DECLARE @shipStartY			tinyint
+DECLARE @shipLength			tinyint
+DECLARE @shipOrientationID	tinyint
 
---if NOT EXISTS(SELECT ShotID FROM Shots WHERE X = @X AND Y = @Y AND RoomID = @RoomID AND UserID = @UserID)
+--Can be derived from known data
+DECLARE @shipEndX		tinyint
+DECLARE @shipEndY		tinyint
+DECLARE @changeX		int
+DECLARE @changeY		int
+
+DECLARE @shipID int
+
+SELECT	@shipStartX			=	us.X,
+		@shipStartY			=	us.Y,
+		@shipLength			=	s.ShipLength,
+		@shipOrientationID	=	us.ShipOrientationID
+FROM	UserShips	us
+JOIN	Ships		s
+ON		s.ShipID			=	us.ShipID
+WHERE	us.RoomID			=	@RoomID
+AND		us.UserID			=	@UserID
+AND		s.ShipID			=	@shipID
+
+
+SELECT	@changeX	=	OrientationValueX,
+		@changeY	=	OrientationValueY
+FROM ShipOrientations
+WHERE ShipOrientationID	=	@shipOrientationID
+
+SET @shipEndX	=	@shipStartX + (@shipLength - 1) * @changeX
+SET @shipEndY	=	@shipStartY	+ (@shipLength - 1) * @changeY
+
+IF(@shipEndX < @shipStartX)
+BEGIN
+	DECLARE @tempX int	=	@shipStartX
+	SET		@shipStartX = @shipEndX
+	SET		@shipEndX = @tempX
+END
+
+IF(@shipEndY < @shipStartY)
+BEGIN
+	DECLARE @tempY int	=	@shipStartY
+	SET		@shipStartY =	@shipEndY
+	SET		@shipEndY	=	@tempY
+END
+
+IF EXISTS(	SELECT 1 AS IsHit
+			WHERE	(@ShotX BETWEEN @shipStartX AND @shipEndX)
+			AND		(@ShotY BETWEEN @shipStartY AND @shipEndY))
+BEGIN
+	print 'Hit'
+END
+ELSE
+BEGIN
+	print 'Miss'
+END
 	
+
+
 
 GO
--- SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
@@ -1,10 +1,11 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 9/1/2017 3:57:23 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 9/29/2017 1:53:56 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 CREATE PROC [dbo].[usp_PlaceShip]
 	@ShipID				int
@@ -14,10 +15,10 @@ CREATE PROC [dbo].[usp_PlaceShip]
 ,	@Y					tinyint
 ,	@ShipOrientationID	int
 as
-if NOT EXISTS(SELECT ShipID FROM UserShips WHERE UserID = @UserID)
+if NOT EXISTS(SELECT ShipID FROM UserShips WHERE UserID = @UserID AND RoomID = @RoomID AND ShipID = @ShipID)
 	BEGIN
 		INSERT INTO UserShips
-		VALUES (@ShipID, @UserID, @RoomID, @X, @Y, @ShipOrientationID)
+		VALUES (@ShipID, @UserID, @RoomID, @X, @Y, @ShipOrientationID, 0)
 	END
 
 GO
