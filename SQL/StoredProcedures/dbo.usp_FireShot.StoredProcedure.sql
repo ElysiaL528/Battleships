22c8405e83780c6aff64d4ec9180b985a5093bb9
ElysiaL528 <elysialopez528@gmail.com>
2018-02-02 12:45:37 -0800

    Fixed FireShot procedure.

--- SQL/StoredProcedures/dbo.usp_FireShot.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_FireShot.StoredProcedure.sql
@@ -1,6 +1,6 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_FireShot]    Script Date: 10/27/2017 11:38:34 AM ******/
+/****** Object:  StoredProcedure [dbo].[usp_FireShot]    Script Date: 2/2/2018 11:39:05 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,17 +10,21 @@ GO
 
 
 
+
+
+
 CREATE PROC [dbo].[usp_FireShot]
-	@UserID		int
-,	@RoomID		int
+	@UserID			int
+,	@RoomID			int
 ,	@ShotX			int	
 ,	@ShotY			int		
+,	@IsTestData		bit
 as
 
 IF NOT EXISTS(SELECT * FROM Shots WHERE @ShotX = X AND @ShotY = Y)
 BEGIN
 
-	DECLARE @hitShipID int = (SELECT	ShipID	FROM	UserShips	WHERE	(dbo.fn_intersects(@UserID, @RoomID, ShipID, @ShotX, @ShotY)) = 1)
+	DECLARE @hitShipID int = (SELECT	ShipID	FROM	UserShips	WHERE	(dbo.fn_intersects(@UserID, @RoomID, ShipID, @ShotX, @ShotY)) = 1 AND RoomID = @RoomID AND UserID = @UserID)
 
 	DECLARE @isHit bit = 0
 
@@ -34,10 +38,13 @@ BEGIN
 	print @isHit
 
 	INSERT INTO Shots
-	VALUES (@UserID, @RoomID, @ShotX, @ShotY, @isHit)
+	VALUES (@UserID, @RoomID, @ShotX, @ShotY, @isHit, @IsTestData)
 
 	RETURN @isHit
 END 
+
+
+
 
 
 
