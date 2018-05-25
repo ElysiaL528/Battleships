9f02f3100ac8beaaa867dc337a56ddfe2f601af1
ElysiaL528 <elysialopez528@gmail.com>
2018-05-25 12:37:35 -0700

    Fixed GetShips stored procedure bug so that it retrieves the ship type id.

--- SQL/StoredProcedures/dbo.usp_GetShips.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_GetShips.StoredProcedure.sql
@@ -1,20 +1,30 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_GetShips]    Script Date: 8/30/2017 3:12:41 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_GetShips]    Script Date: 5/25/2018 11:50:51 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+
+
 CREATE PROC [dbo].[usp_GetShips]
 	@RoomID	int
 ,	@UserID	int
 as
 	SELECT	UserShips.X
 ,			UserShips.Y
-,			UserShips.ShipOrientationID
+,			ShipOrientations.ShipOrientationID
 ,			Ships.ShipLength
+,			UserShips.HitCount
 	FROM	UserShips
 	JOIN	Ships
-	ON		UserShips.ShipID	=	Ships.ShipID
-	WHERE UserID	=	@UserID
+	ON		UserShips.ShipTypeID	=	Ships.ShipID
+	JOIN ShipOrientations
+	ON UserShips.ShipOrientationID = ShipOrientations.ShipOrientationID
+	WHERE UserID	=	@UserID 
+	AND RoomID		=	@RoomID
+
+
+
 GO
