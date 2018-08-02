1c290e40505979c0872bbd62ec4ed7db64187a48
ElysiaL528 <elysialopez528@gmail.com>
2018-08-01 17:30:26 -0700

    Changed CheckUsersReady function to only check the user's opponent ready status.

--- SQL/StoredProcedures/dbo.usp_CheckUsersReady.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_CheckUsersReady.StoredProcedure.sql
@@ -1,15 +1,30 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_CheckUsersReady]    Script Date: 9/1/2017 3:57:23 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_CheckUsersReady]    Script Date: 8/1/2018 5:29:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 CREATE PROC [dbo].[usp_CheckUsersReady]
-@RoomID		int
+	@UserID		int
+,	@RoomID		int
 as
-SELECT	isHostPlayerReady, isJoinedPlayerReady
-FROM	Rooms 
-WHERE	RoomID	=	@RoomID
+DECLARE @HostPlayerID int = (SELECT HostPlayerID FROM Rooms WHERE RoomID = @RoomID)
+DECLARE @JoinedPlayerID int = (SELECT JoinedPlayerID FROM Rooms WHERE RoomID = @RoomID)
+
+if(@HostPlayerID = @UserID)
+BEGIN
+	SELECT isJoinedPlayerReady
+	FROM Rooms
+	WHERE RoomID = @RoomID
+END
+else
+BEGIN
+	SELECT	isHostPlayerReady
+	FROM	Rooms 
+	WHERE	RoomID	=	@RoomID
+END
+
 
 GO
