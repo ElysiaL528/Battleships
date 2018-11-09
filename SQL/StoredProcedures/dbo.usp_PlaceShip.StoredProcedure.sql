9811ebd311bb94602cff95daaf641841716071a8
ElysiaL528 <elysialopez528@gmail.com>
2018-11-09 14:46:39 -0800

    Worked on randomizing function. Also found bug in SQL where placing a ship at (1,1) with the ship orientation being up was valid. Next, debug.

--- SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
+++ SQL/StoredProcedures/dbo.usp_PlaceShip.StoredProcedure.sql
@@ -1,10 +1,13 @@
 USE [ElysiaLopezBattleships2017]
 GO
-/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 2/23/2018 12:45:31 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_PlaceShip]    Script Date: 11/9/2018 2:44:39 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+
+
 
 
 
@@ -19,7 +22,6 @@ CREATE PROC [dbo].[usp_PlaceShip]
 ,	@X					int
 ,	@Y					int
 ,	@ShipOrientationID	int
-,	@IsTestData			bit
 as
 
 DECLARE @ErrorMessage varchar(21)		
@@ -50,66 +52,71 @@ SET @EndY	=	@Y + (@shipLength - 1) * @changeY
 
 DECLARE @HostPlayerID int = (SELECT HostPlayerID FROM Rooms WHERE RoomID = @RoomID)
 DECLARE @JoinedPlayerID int = (SELECT JoinedPlayerID FROM Rooms WHERE RoomID = @RoomID)
+DECLARE @isRoomTestData bit = (SELECT isTestData FROM Rooms WHERE RoomID = @RoomID)
 
 
 IF (@HostPlayerID != @UserID AND @JoinedPlayerID != @UserID)
 	BEGIN
 	SET @ErrorMessage = 'Player is not in room'
+	SELECT @ErrorMessage
 	END
 ELSE
 BEGIN
 	IF (@EndX > 10 OR @EndX < 0) OR (@EndY > 10 OR @EndY < 0)
 	BEGIN
 		SET @ErrorMessage = 'Off the board'
+		SELECT @ErrorMessage
 	END
 	ELSE
 	BEGIN
+		IF @EndX < @X
+		BEGIN
+			DECLARE @tempX int = @EndX
+			SET @EndX = @X
+			SET @X = @tempX
+		END
 
-	IF @EndX < @X
-	BEGIN
-		DECLARE @tempX int = @EndX
-		SET @EndX = @X
-		SET @X = @tempX
-	END
-
-	IF @EndY < @Y
-	BEGIN
-		DECLARE @tempY int = @EndY
-		SET @EndY = @Y
-		SET @Y = @tempY
-	END
-	if NOT EXISTS(SELECT ShipID FROM UserShips WHERE UserID = @UserID AND RoomID = @RoomID AND ShipTypeID = @ShipTypeID)
+		IF @EndY < @Y
+		BEGIN
+			DECLARE @tempY int = @EndY
+			SET @EndY = @Y
+			SET @Y = @tempY
+		END
+		if NOT EXISTS(SELECT ShipID FROM UserShips WHERE UserID = @UserID AND RoomID = @RoomID AND ShipTypeID = @ShipTypeID)
 		BEGIN
 
-		DECLARE	@StartCoordIsOverlapping	int =	(SELECT TOP 1 ShipID
-											FROM	UserShips WHERE (dbo.fn_intersects(@UserID, @RoomID, ShipID, @X, @Y))= 1 AND RoomID = @RoomID)
+			DECLARE	@StartCoordIsOverlapping	int =	(SELECT TOP 1 ShipID
+												FROM	UserShips WHERE (dbo.fn_intersects(@UserID, @RoomID, ShipID, @X, @Y))= 1 AND RoomID = @RoomID)
 
-		DECLARE	@EndCoordIsOverlapping	int =	(SELECT TOP 1 ShipID
-											FROM	UserShips WHERE (dbo.fn_intersects(@UserID, @RoomID, ShipID, @EndX, @EndY)) = 1 AND RoomID = @RoomID)
+			DECLARE	@EndCoordIsOverlapping	int =	(SELECT TOP 1 ShipID
+												FROM	UserShips WHERE (dbo.fn_intersects(@UserID, @RoomID, ShipID, @EndX, @EndY)) = 1 AND RoomID = @RoomID)
 
-		IF(@StartCoordIsOverlapping IS NULL AND @EndCoordIsOverlapping IS NULL)
-			BEGIN
-				INSERT INTO UserShips
-				VALUES (@UserID, @RoomID, @ShipTypeID, @X, @Y, @ShipOrientationID, 0, 0, 0)
+			IF(@StartCoordIsOverlapping IS NULL AND @EndCoordIsOverlapping IS NULL)
+				BEGIN
+					INSERT INTO UserShips
+					VALUES (@UserID, @RoomID, @ShipTypeID, @X, @Y, @ShipOrientationID, 0, 0, @isRoomTestData)
 
-				SET @ErrorMessage = 'Placed ship'
-			END
-		ELSE
-			BEGIN
-				SET @ErrorMessage = 'Overlapping ship'
-			END
+					SET @ErrorMessage = 'Placed ship'
+				END
+			ELSE
+				BEGIN
+					SET @ErrorMessage = 'Overlapping ship'
+				END
 		END
-	ELSE
+		ELSE
 		BEGIN
 			SET @ErrorMessage = 'Pre-existing ship'
 		END
 
-	--SELECT dbo.fn_intersects(2, 22, ShipID, 1, 1), RoomID, UserID FROM UserShips
-
+		--SELECT dbo.fn_intersects(2, 22, ShipID, 1, 1), RoomID, UserID FROM UserShips
+		SELECT @ErrorMessage
+		END
 	END
-END
 
-SELECT @ErrorMessage
+
+
+
+
 
 
 
